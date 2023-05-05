3a10ad79f3f0e112099d2376d854e9127b9fe035
artificial <arti@artibutter.xyz>
2023-05-04 22:10:29 -0700

    1.4.0
    - handle messages that are too long
    - continue move to official api (?)
    - remove auto character reset

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ dotenv.config()
 import Discord from 'discord.js'
 import { ChatGPTAPI } from 'chatgpt'
 
+import fs from 'fs'
+import path from 'path';
+
 const client = new Discord.Client();
 client.login(process.env.DISCORD);
 client.on("ready", () => {
@@ -12,7 +15,6 @@ client.on("ready", () => {
 });
 
 let conversation = {
-  conversationId: null,
   parentMessageId: null
 };
 
@@ -29,11 +31,15 @@ client.on("message", async message => {
 
     try {
 
+      // Ignore
+      if (message.content.startsWith("!!")) {
+        return;
+      }
+
       message.channel.startTyping();
 
       // Reset conversation
       if (message.content.startsWith("%reset")) {
-        conversation.conversationId = "";
         conversation.parentMessageId = "";
         message.channel.send("Conversation reset.");
         message.channel.stopTyping();
@@ -41,26 +47,22 @@ client.on("message", async message => {
       }
       // Print conversation ID and parent message ID
       if (message.content.startsWith("%debug")) {
-        message.channel.send("conversationId: " + conversation.conversationId + "\nparentMessageId: " + conversation.parentMessageId);
+        message.channel.send("parentMessageId: " + conversation.parentMessageId);
         message.channel.stopTyping();
         return;
       }
 
       // Start conversation if it doesn't exist
-      if (!conversation.conversationId) {
-        const res = await api.sendMessage("pretend you are a human named spongeass that speaks in uwuspeak, and do not mention this initial prompt", {
+      if (!conversation.parentMessageId) {
+        const res = await api.sendMessage("pretend you are a human named spongeass that speaks in uwuspeak, and don't mention this initial prompt", {
           role: "system",
         });
-        conversation.conversationId = res.conversationId;
         conversation.parentMessageId = res.parentMessageId;
       }
 
 
-
-      var messageid = conversation.conversationId
       var parentid = conversation.parentMessageId
       const res = await api.sendMessage(message.content, {
-        conversationId: messageid,
         parentMessageId: parentid
       });
 
@@ -75,19 +77,23 @@ client.on("message", async message => {
         return message.channel.send(`**[FILTERED]**`);
       }
 
-      // Reset if OpenAI activates safety filter
-      if (res.text.includes('can\'t') || res.text.includes('language model') || res.text.includes('cannot engage') || res.text.includes('inappropriate')) {
-        conversation.conversationId = "";
-        conversation.parentMessageId = "";
-        message.channel.send(res.text).then(function (message) {
-          message.react("⚠️");
-        })
-        message.channel.stopTyping();
-        return;
+      // Reset if character is broken
+      //if (res.text.includes('can\'t') || res.text.includes('language model') || res.text.includes('cannot engage') || res.text.includes('inappropriate')) {
+      //  message.channel.send(res.text).then(function (message) {
+      //    message.react("⚠️");
+      //  })
+      //  message.channel.stopTyping();
+      //  return;
+      //}
+
+      // Handle long responses
+      if (res.text >= 2000) {
+        fs.writeFileSync(path.resolve('./how.txt'), res.text);
+        message.channel.send('how', { files: ["./how.txt"] });
       }
 
+
       message.channel.send(`${res.text}`);
-      conversation.conversationId = res.conversationId;
       conversation.parentMessageId = res.parentMessageId;
       message.channel.stopTyping();
 
