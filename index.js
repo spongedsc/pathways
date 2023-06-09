ac913d49accd72f029046f32df669dd33e1d8b75
artificial <59352535+artificialbutter@users.noreply.github.com>
2023-06-08 19:16:57 -0700

    move to character.ai to use a custom model

--- index.js
+++ index.js
@@ -2,92 +2,78 @@ import * as dotenv from 'dotenv'
 dotenv.config()
 
 import Discord from 'discord.js'
-import { ChatGPTAPI } from 'chatgpt'
 
 import fs from 'fs'
 import path from 'path';
 
+import CharacterAI from 'node_characterai';
+
 const client = new Discord.Client();
+const characterAI = new CharacterAI();
 
-client.on("ready", () => {
-  console.log(`Ready! Logged in as ${client.user.tag}`);
-  client.user.setActivity(`uwu`);
-});
+(async () => {
 
-let conversation = {
-  parentMessageId: null
-};
+  await characterAI.authenticateAsGuest();
 
-const api = new ChatGPTAPI({
-  apiKey: process.env.OPENAI_API_KEY
-})
+  const characterId = process.env.CHARACTER_ID
 
-client.on("message", async message => {
+  client.on("ready", () => {
+    console.log(`Ready! Logged in as ${client.user.tag}`);
+    client.user.setActivity(`uwu`);
+  });
 
-  if (message.channel.id == process.env.CHANNELID) {
-    console.log
-    if (message.author.bot) return;
-    if (!message.content) return;
+  client.on("message", async message => {
 
-    try {
+    if (message.channel.id == process.env.CHANNELID) {
+      console.log
+      if (message.author.bot) return;
+      if (!message.content) return;
 
-      // Ignore messages starting with !!
-      if (message.content.startsWith("!!")) {
-        return;
+      try {
+
+        // Ignore messages starting with !!
+        if (message.content.startsWith("!!")) {
+          return;
+        }
+
+        message.channel.startTyping();
+
+        const chat = await characterAI.createOrContinueChat(characterId);
+
+        const res = await chat.sendAndAwaitResponse(message.content, true);
+
+
+        // Filter @everyone and @here
+        if (res.text.includes(`@everyone`)) {
+          message.channel.stopTyping();
+          return message.channel.send(`**[FILTERED]**`);
+        }
+        if (res.text.includes(`@here`)) {
+          message.channel.stopTyping();
+          return message.channel.send(`**[FILTERED]**`);
+        }
+
+        // Handle long responses
+        if (res.text.length >= 2000) {
+          fs.writeFileSync(path.resolve('./how.txt'), res.text);
+          message.channel.send('how', { files: ["./how.txt"] });
+          message.channel.stopTyping();
+          return;
+        }
+
+
+        message.channel.send(`${res.text}`);
+
+        message.channel.stopTyping();
+
+      } catch (error) {
+        console.log(error)
+        message.channel.stopTyping();
+        return message.channel.send(`\`${error}\``);
       }
 
-      message.channel.startTyping();
-
-      // Reset conversation
-      if (message.content.startsWith("%reset")) {
-        conversation.parentMessageId = null;
-        message.channel.send("Conversation reset.");
-        message.channel.stopTyping();
-        return;
-      }
-      // Print conversation ID and parent message ID
-      if (message.content.startsWith("%debug")) {
-        message.channel.send("parentMessageId: " + conversation.parentMessageId);
-        message.channel.stopTyping();
-        return;
-      } 
-
-      var parentid = conversation.parentMessageId
-      const res = await api.sendMessage(message.content, {
-        parentMessageId: parentid
-      });
-
-
-      // Filter @everyone and @here
-      if (res.text.includes(`@everyone`)) {
-        message.channel.stopTyping();
-        return message.channel.send(`**[FILTERED]**`);
-      }
-      if (res.text.includes(`@here`)) {
-        message.channel.stopTyping();
-        return message.channel.send(`**[FILTERED]**`);
-      }
-
-      // Handle long responses
-      if (res.text.length >= 2000) {
-        fs.writeFileSync(path.resolve('./how.txt'), res.text);
-        message.channel.send('how', { files: ["./how.txt"] });
-        message.channel.stopTyping();
-        return;
-      }
-
-
-      message.channel.send(`${res.text}`);
-      conversation.parentMessageId = res.parentMessageId;
-      message.channel.stopTyping();
-
-    } catch (error) {
-      console.log(error)
-      message.channel.stopTyping();
-      return message.channel.send(`\`${error}\``);
     }
+  });
 
-  }
-});
-
+})()
 client.login(process.env.DISCORD);
