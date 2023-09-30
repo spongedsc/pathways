a44672497920890ce521be6143dba093159bc65a
artificial <59352535+artificialbutter@users.noreply.github.com>
2023-09-30 14:06:57 -0700

    reasonable settings again

--- index.js
+++ index.js
@@ -80,8 +80,8 @@ async function sendChat(userInput, history) {
 
     preset: 'None',
     do_sample: true,
-    temperature: 1.99, // set to 1 for extra fun!!! weeeee (0.7 is default)
-    top_p: 1,
+    temperature: 0.82, // set to 1 for extra fun!!! weeeee (0.7 is default)
+    top_p: 0.21,
     typical_p: 1,
     epsilon_cutoff: 0,
     eta_cutoff: 0,
@@ -89,7 +89,7 @@ async function sendChat(userInput, history) {
     top_a: 0,
     repetition_penalty: 3,
     repetition_penalty_range: 0,
-    top_k: 200,
+    top_k: 72,
     min_length: 0,
     no_repeat_ngram_size: 0,
     num_beams: 1,
