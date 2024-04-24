9ace93057b903492a99b66f60df340a0905574a5
artifishvr <59352535+artifishvr@users.noreply.github.com>
2024-04-23 23:27:49 -0700

    improve backend error

--- index.js
+++ index.js
@@ -29,10 +29,7 @@ const client = new Client({
 });
 
 backendsocket.on("connect_error", (err) => {
-  // the reason of the error, for example "xhr poll error"
-  console.log(err.message);
-  // some additional description, for example the status code of the initial HTTP response
-  console.log(err.description);
+  console.error(`Error connecting to backend: ${err.message}`);
 });
 
 if (!fs.existsSync('./temp')) fs.mkdirSync('./temp');
