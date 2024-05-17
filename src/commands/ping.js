4981a1bc2af02349392c7f6a64154aecd22b03dd
Dani <git@danny.works>
2024-05-16 23:17:02 -0700

    ping command

--- src/commands/ping.js
+++ src/commands/ping.js
@@ -1,10 +1,36 @@
+import { EmbedBuilder } from "discord.js";
+import { Duration } from "luxon";
+
 /** @type {import('./index.js').Command} */
 export default {
 	data: {
-		name: 'ping',
-		description: 'Ping!',
+		name: "ping",
+		description: "Ping!",
 	},
 	async execute(interaction) {
-		await interaction.reply("It's working!");
+		const apiStart = Temporal.Instant.fromEpochMilliseconds(interaction.createdTimestamp);
+		const apiEnd = Temporal.Now.instant();
+		await interaction.deferReply({ ephemeral: true });
+
+		const apiDiff = apiEnd.since(apiStart).milliseconds + "ms";
+		const uptime = Duration.fromISO(
+			Temporal.Duration.from({ seconds: Math.floor(process.uptime()) }).toString(),
+		).toHuman({ unitDisplay: "narrow" });
+
+		const content =
+			` 🌐 **Round-Trip Latency** \`${apiDiff}\`` +
+			"\n" +
+			` 🛜 **Socket Latency** \`${interaction.client.ws.ping}ms\`` +
+			"\n" +
+			` ⌚ **Uptime** \`${uptime}\`` +
+			"\n";
+
+		const SendEmbed = new EmbedBuilder()
+			.setTitle("Current statistics")
+			.setColor("Random")
+			.setDescription(content)
+			.setTimestamp();
+
+		return await interaction.editReply({ embeds: [SendEmbed] });
 	},
 };
