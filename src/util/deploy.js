23bd0f6bc10bef124dc3186e565abce9ae06fe4c
Dani <git@danny.works>
2024-05-17 19:26:05 -0700

    add actions to rollout handler

--- src/util/deploy.js
+++ src/util/deploy.js
@@ -11,9 +11,8 @@ export const cmdRollout = async () => {
 	const env = new Environment();
 
 	const commands = await loadCommands(new URL("../commands/", import.meta.url));
-	const commandData = [...commands.values()]
-		.filter((c) => !c.type || c.type === "command")
-		.map((command) => command.data);
+	const actions = await loadCommands(new URL("../actions/", import.meta.url));
+	const data = [...commands.values(), ...actions.values()].map((command) => command.data);
 
 	const rest = new REST({ version: "10" }).setToken(process.env.DISCORD_TOKEN);
 	const api = new API(rest);
@@ -23,11 +22,11 @@ export const cmdRollout = async () => {
 			? await api.applicationCommands.bulkOverwriteGuildCommands(
 					process.env.APPLICATION_ID,
 					process.env.DEV_GUILD,
-					commandData,
+					data,
 				)
-			: await api.applicationCommands.bulkOverwriteGlobalCommands(process.env.APPLICATION_ID, commandData);
+			: await api.applicationCommands.bulkOverwriteGlobalCommands(process.env.APPLICATION_ID, data);
 
 	console.log(
-		`${chalk.bold.green("Core")} Successfully registered ${chalk.bold(result.length)} commands (${Temporal.Now.instant().toLocaleString("en-GB", { timeZone: "Etc/UTC", timeZoneName: "short" })})`,
+		`${chalk.bold.green("Core")} Successfully registered ${chalk.bold(result.length)} commands/actions (${Temporal.Now.instant().toLocaleString("en-GB", { timeZone: "Etc/UTC", timeZoneName: "short" })})`,
 	);
 };
