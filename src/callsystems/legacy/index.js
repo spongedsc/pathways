c93f7cc6caeafe3d110c03b75b27814d275310b2
Dani <git@danny.works>
2024-05-20 22:32:42 -0700

    ♻️ Migrate Legacy callsystem to Callsystems API

---
+++ src/callsystems/legacy/index.js
@@ -0,0 +1,80 @@
+import { ModelInteractions } from "../../util/models/index.js";
+import { Callsystem } from "../../lib/callsystems/index.js";
+
+export default class Legacy extends Callsystem {
+	constructor(opts) {
+		const { env, message, client, defaultModel, defaultProvider } = opts || {};
+		super({ env, message, client, defaultModel, defaultProvider });
+	}
+
+	static get packageId() {
+		return "cs.spongedsc.legacy";
+	}
+
+	static get name() {
+		return "Legacy";
+	}
+
+	static get version() {
+		return "2.0.1";
+	}
+
+	static get capabilities() {
+		return ["legacy"];
+	}
+
+	async activate() {
+		const { message, client, env } = this;
+		const modelInteractions = new ModelInteractions({
+			message,
+			kv: client.kv,
+			instructionSet: client.tempStore.get("instructionSet") || env.MODEL_LLM_PRESET || "default",
+			baseHistory: [],
+			accountId: env.CLOUDFLARE_ACCOUNT_ID,
+			token: env.CLOUDFLARE_ACCOUNT_TOKEN,
+			model: "@cf/meta/llama-3-8b-instruct",
+		});
+
+		const formattedMessage = await modelInteractions.response.formatUserMessage();
+		const validityCheck = await modelInteractions.messageEvent.validateHistory();
+
+		if (!validityCheck.valid) {
+			if (validityCheck?.handled?.isRequired && !validityCheck?.handled?.executed)
+				return await message.react("⚠️").catch(() => {
+					console.log("Failed to delete key from KV after valid history check failed");
+					console.error(e);
+					return false;
+				});
+		}
+
+		const history = await modelInteractions.history
+			.add({
+				key: message?.channel?.id,
+				role: "user",
+				content:
+					formattedMessage +
+					(validityCheck?.valid
+						? ""
+						: "\n\nAlso, you had a system error and your memory was cleared. Explain that to me."),
+				respondingTo: message?.id,
+			})
+			.catch(console.error);
+
+		const { textResponse, genData, callResponse } = await modelInteractions.messageEvent.handleTextModelCall({
+			history,
+		});
+
+		if (callResponse.length === 0 || callResponse === "") return await message.react("⚠️").catch(() => false);
+
+		const { responseMsg, events } = await modelInteractions.messageEvent.createResponse({
+			textResponse,
+			conditions: {
+				amnesia: !validityCheck?.valid && validityCheck?.handled?.isRequired && validityCheck?.handled?.executed,
+				imagine: callResponse.includes("!gen"),
+			},
+		});
+
+		if (responseMsg && callResponse.includes("!gen"))
+			return await modelInteractions.messageEvent.handleImageModelCall({ genData, textResponse, responseMsg, events });
+	}
+}
