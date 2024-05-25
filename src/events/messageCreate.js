901512cc397baf5b7b3bb2dc4a0ffa67d4494c55
Dani <git@danny.works>
2024-05-25 16:51:33 -0700

    callsystem fallback handling, add variable to activation response for future debug

--- src/events/messageCreate.js
+++ src/events/messageCreate.js
@@ -2,12 +2,25 @@ import { ChannelType, Events } from "discord.js";
 import { CallsystemStd, predicate } from "../lib/callsystems/index.js";
 
 const callTextChannel = async ({ client, message }) => {
-	const callsystem = client.callsystemsMap.get("cs.spongedsc.legacy-2.0.1");
-	if (!callsystem || !predicate(callsystem)) return;
+	const callsystem = client.callsystemsMap.get(client.tempStore.get("callsystem") || "cs.spongedsc.legacy-latest");
+	if (!callsystem || !predicate(callsystem)) {
+		await message
+			.reply("**⚠️ No callsystem was found. Please contact an administrator for further assistance.**")
+			.catch(() => {
+				console.log("Failed to send callsystem error to user");
+				console.error(e);
+				return false;
+			});
+		return {
+			success: false,
+			message: "No callsystem found.",
+			context: {},
+		};
+	}
 	if (!CallsystemStd.conditions(message, process.env)) return;
 
 	const callsystemInstance = new callsystem({ env: process.env, message, client });
-	await callsystemInstance.activate();
+	const activationResponse = await callsystemInstance.activate();
 };
 
 /** @type {import('./index.js').Event<Events.MessageCreate>} */
