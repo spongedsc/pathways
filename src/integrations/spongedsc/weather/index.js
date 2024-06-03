0e78af5f8152e3bcf3de00e9d398b707d8bbe5d4
Dani <git@danny.works>
2024-06-02 20:25:52 -0700

    weather woohoo

--- src/integrations/spongedsc/weather/index.js
+++ src/integrations/spongedsc/weather/index.js
@@ -1,6 +1,9 @@
 import { z } from "zod";
 import { Integration } from "../../../callsystems/spongedsc/integrations/lib/class.js";
 import { tool } from "ai";
+import { fetch } from "undici";
+import dedent from "dedent";
+import { codes as wmo } from "./wmo.data.js";
 export default class HelloWorld extends Integration {
 	constructor(opts) {
 		const { env, message, client, std, provider } = opts || {};
@@ -44,12 +47,49 @@ export default class HelloWorld extends Integration {
 	async activate({ arguments: args }) {
 		const { env, message, client } = this;
 
+		const pois = await fetch(
+			`https://nominatim.openstreetmap.org/search?addressdetails=1&q=${args.location}&format=jsonv2&limit=1`,
+		)
+			.then((res) => res.json())
+			.catch((err) => {
+				console.log(err);
+				return [];
+			});
+
+		if (pois.length === 0) {
+			return {
+				success: false,
+				messages: [
+					{
+						role: "tool",
+						content: `The weather in ${args.location} is unknown`,
+					},
+				],
+				data: {},
+			};
+		}
+
+		const weather = await fetch(
+			`https://api.open-meteo.com/v1/forecast?latitude=${pois[0]?.lat}&longitude=${pois[0]?.lon}&current=temperature_2m,is_day,weather_code&daily=weather_code,temperature_2m_max,temperature_2m_min&timezone=auto&forecast_days=1`,
+		)
+			.then((res) => res.json())
+			.catch((err) => {
+				console.log(err);
+				return {};
+			});
+
+		const isDay = weather?.current?.is_day === 1;
+
 		return {
 			success: true,
 			messages: [
 				{
 					role: "tool",
-					content: `The weather in ${args.location} is 17 degrees celsius. It is raining`,
+					content: dedent`
+                    The weather in ${pois[0]?.display_name} is currently ${weather?.current?.temperature_2m || "<unknown>"}°C and ${wmo[weather?.current?.weather_code]?.[isDay ? "day" : "night"] || "<unknown>"}.
+                    
+                    Today, the weather is expected to be ${wmo[weather?.daily?.weather_code?.[0]]?.[isDay ? "day" : "night"] || "<unknown>"}, with a high of ${weather?.daily?.temperature_2m_max?.[0] || "<unknown>"}°C and a low of ${weather?.daily?.temperature_2m_min?.[0] || "<unknown>"}°C.
+                    `,
 				},
 			],
 			data: {},
